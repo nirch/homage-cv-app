302535fcdab46a7de542a87b1885470457af9cc8
Nir Channes <nir@homage.it>
2016-03-20 14:25:11 +0200

    gpw mk file for Android ndk-build

--- VimGui/Gpw/Gpw.mk
+++ VimGui/Gpw/Gpw.mk
@@ -2,20 +2,25 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(UNAME),Windows_NT)
 LOCAL_PATH := $(call  my-dir)
 else
-LOCAL_PATH := $(abspath $(call my-dir)/../Gpw/)
+LOCAL_PATH := $(abspath $(call my-dir)/../../Gpw/)
 endif
+
+LOCAL_PATH := $(call  my-dir)
+
 
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES :=   $(LOCAL_PATH)/./.  $(LOCAL_PATH)/../../Core/ImageLib/.  $(LOCAL_PATH)/../../Core/Util/.  $(LOCAL_PATH)/../../VimGui/.  $(LOCAL_PATH)/../../Image/.
 
 LOCAL_SRC_FILES := 	\
-	FrameLabel/FrameLabel.cpp \
-	Gpw.cpp \
-	GpwEllipse.cpp \
-	GpwImage.cpp \
-	GpwMarker.cpp \
-	TextLabel/TextLabel.cpp
+	./FrameBuffer/FrameBuffer.cpp \
+	./FrameLabel/FrameLabel.cpp \
+	./Gpw.cpp \
+	./GpwEllipse.cpp \
+	./GpwPln.cpp \
+	./GpwImage.cpp \
+	./GpwMarker.cpp \
+	./TextLabel/TextLabel.cpp
 
 
 LOCAL_CFLAGS := -w
